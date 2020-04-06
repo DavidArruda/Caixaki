55ea7eb4fde873ed438ef037817b0d5d9d030345
David <deh0_@hotmail.com>
2020-04-06 15:21:42 -0300

    Classes para gerar as tabelas de auditoria

---
+++ src/br/com/project/listener/ContextLoaderListenerCaixakiUtils.java
@@ -0,0 +1,34 @@
+package br.com.project.listener;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+@ApplicationScoped //INSTANCIA UNICA PARA O PROJETO INTEIRO
+public class ContextLoaderListenerCaixakiUtils extends ContextLoader implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	/** 
+	 * Retorna todo o contexto do spring (ambiente de execução)
+	 * @return
+	 */
+	private static WebApplicationContext getWac() {
+		return WebApplicationContextUtils.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
+	}
+	
+	public static Object getBean(String idNomeBean) {
+		return getWac().getBean(idNomeBean);
+		
+	}
+	
+	public static Object getBean(Class<?> classe) {
+		return getWac().getBean(classe);
+		
+	}
+
+}
