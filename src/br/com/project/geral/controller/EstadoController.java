203d24a0766cc8b477665c233cafb93e847bfaa8
David <deh0_@hotmail.com>
2020-04-27 15:37:57 -0300

    Criação EstadoController

---
+++ src/br/com/project/geral/controller/EstadoController.java
@@ -0,0 +1,32 @@
+package br.com.project.geral.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
+import org.springframework.stereotype.Controller;
+
+import br.com.framework.implementacao.crud.InplementacaoCrud;
+import br.com.framework.interfac.crud.InterfaceCrud;
+import br.com.project.model.classes.Estado;
+
+@Controller
+public class EstadoController extends InplementacaoCrud<Estado> implements InterfaceCrud<Estado>{
+
+	private static final long serialVersionUID = 1L;
+	
+	public List<SelectItem> getListEstado() throws Exception{
+		List<SelectItem> list = new ArrayList<>();
+		
+		List<Estado> estados = super.findListByQueryDynamic("from Estado");
+		
+		for (Estado estado : estados) {
+			list.add(new SelectItem(estado, estado.getEst_nome()));
+		}
+		
+		return list;
+	}
+	
+
+}
