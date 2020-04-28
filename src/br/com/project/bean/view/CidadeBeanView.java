83dfaa3055c6250aadc151c274612d8fb55dff39
David <deh0_@hotmail.com>
2020-04-28 14:57:56 -0300

    adicionado metodo de excluir e corrido metódo de atualizar

--- src/br/com/project/bean/view/CidadeBeanView.java
+++ src/br/com/project/bean/view/CidadeBeanView.java
@@ -21,46 +21,52 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 	private static final long serialVersionUID = 1L;
 
 	private Cidade objetoSelecionado = new Cidade();
-	
+
 	private final String url = "/cadastro/cad_cidade.jsf?faces-redirect=true";
-	
+
 	private List<Cidade> list = new ArrayList<>();
-	
+
 	@Autowired
 	private CidadeController cidadeController;
-	
+
 	@Override
 	public String salvar() throws Exception {
 		objetoSelecionado = cidadeController.merge(objetoSelecionado);
 		return "";
 	}
-	
+
 	@Override
 	public String editar() throws Exception {
-		objetoSelecionado = cidadeController.merge(objetoSelecionado);
-		return "";
-	}
-	
-	@Override
-	public String novo() throws Exception {
-		objetoSelecionado = new Cidade();
 		
 		return url;
 	}
 	
 	@Override
+	public void excluir() throws Exception {
+		cidadeController.delete(objetoSelecionado);
+		novo();
+	}
+
+	@Override
+	public String novo() throws Exception {
+		objetoSelecionado = new Cidade();
+
+		return url;
+	}
+
+	@Override
 	public void saveNotReturn() throws Exception {
 		System.out.println(objetoSelecionado.getCid_descricao());
 	}
-	
+
 	public void setObjetoSelecionado(Cidade objetoSelecionado) {
 		this.objetoSelecionado = objetoSelecionado;
 	}
-	
+
 	public Cidade getObjetoSelecionado() {
 		return objetoSelecionado;
 	}
-	
+
 	public List<Cidade> getList() throws Exception {
 		list = cidadeController.findList(Cidade.class);
 		return list;
