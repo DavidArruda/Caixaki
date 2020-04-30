b6c1403f1477236173b947b078e79fb85e5498a4
David <deh0_@hotmail.com>
2020-04-30 13:33:04 -0300

    Adicionado metodo refresh, que limpara o managedBean  resposavel pela tela e com isso será corrigio o erro duplicar dados apos atualizar a tela.

--- src/br/com/project/util/all/BeanViewAbstract.java
+++ src/br/com/project/util/all/BeanViewAbstract.java
@@ -1,5 +1,9 @@
 package br.com.project.util.all;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +17,30 @@ import org.springframework.stereotype.Component;
 public class BeanViewAbstract implements ActionViewPadrao { // ABSTRACT OBRIGA QUE AS OUTRAS CLASSES IMPLEMENTE
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 *  irá fazer o RELOAD forcado da tela JSF e isso irá limpar os dados em cache evitando a duplicação,
+	 * esse método devera ser chamado de dentro do método de sucesso,
+	 * assim a correção fica correta para todo o sistema. 
+	 * @throws Exception
+	 */
+	protected void refresh() throws Exception {
+		  /*Pega o contexto do JSF*/
+		  ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		  
+		  /*Pegas informações da solicitação e HTTP*/
+		  StringBuffer requestURL = ((HttpServletRequest) ec.getRequest()).getRequestURL();
+		  
+		  /*Pega a URL completa da soliticação*/
+		  String queryString = ((HttpServletRequest) ec.getRequest()).getQueryString();
+		  
+		  /*Permite mostrar as menssagens após redirecionamento*/
+		  ec.getFlash().setKeepMessages(true);
+		 
+		  /*Faz o refresh da página JSF*/
+		  ec.redirect((queryString == null) ? requestURL.toString() :
+			  requestURL.append('?').append(queryString).toString());
+  	}
 
 	@Override
 	public void limparLista() throws Exception {
@@ -76,6 +104,7 @@ public class BeanViewAbstract implements ActionViewPadrao { // ABSTRACT OBRIGA Q
 	 */
 	protected void sucesso() throws Exception {
 		statusOperation(StatusPersistencia.SUCESSO);
+		refresh();
 	}
 
 	protected void error() throws Exception {
