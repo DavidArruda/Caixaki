4d3b800ee1332ef2e8f05dab31ddef53813fe3b9
David <deh0_@hotmail.com>
2020-05-27 22:46:14 -0300

    Criação managed ben view para mensagens.

--- src/br/com/project/bean/view/MensagemBeanView.java
+++ src/br/com/project/bean/view/MensagemBeanView.java
@@ -1,12 +1,19 @@
 package br.com.project.bean.view;
 
 import javax.faces.bean.ManagedBean;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.framework.interfac.crud.InterfaceCrud;
 import br.com.project.bean.geral.BeanManagedViewAbstract;
+import br.com.project.geral.controller.EntidadeController;
+import br.com.project.model.classes.Entidade;
+import br.com.project.model.classes.Mensagem;
 
 @Controller
 @Scope(value = "session")
@@ -15,10 +22,31 @@ public class MensagemBeanView extends BeanManagedViewAbstract{
 
 	private static final long serialVersionUID = 1L;
 	
+	private Mensagem objetoSelecionado = new Mensagem();
+	
+	@Autowired
+	private ContextoBean contextoBean;
+	
+	@Autowired
+	private EntidadeController entidadeController;
+	
 	@Override
 	public String novo() throws Exception {
-		System.out.println("Chamou método novo mensagem");
+		objetoSelecionado = new Mensagem();
+		objetoSelecionado.setUsr_origem(contextoBean.getEntidadeLogada());
 		return "";
+	}
+	
+	@RequestMapping("**/buscarUsuarioDestinoMsg")
+	public void buscarUsuarioDestinoMsg(HttpServletResponse httpServletResponse,
+			@RequestParam(value = "codEntidade") Long codEntidade) throws Exception{
+		
+		Entidade entidade = entidadeController.findObjById(Entidade.class, codEntidade);
+		
+		if (entidade != null) {
+			objetoSelecionado.setUsr_destino(entidade);
+			httpServletResponse.getWriter().write(entidade.getJson().toString());
+		}
 	}
 
 	@Override
@@ -30,11 +58,21 @@ public class MensagemBeanView extends BeanManagedViewAbstract{
 	protected InterfaceCrud<?> getControler() {
 		return null;
 	}
+	
+	public Mensagem getObjetoSelecionado() {
+		return objetoSelecionado;
+	}
+	
+	public void setObjetoSelecionado(Mensagem objetoSelecionado) {
+		this.objetoSelecionado = objetoSelecionado;
+	}
 
 	@Override
 	public String condicaoAndParaPesquisa() throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 
 }
