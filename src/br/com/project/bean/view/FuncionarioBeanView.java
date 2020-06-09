62056b37f4a5a505f5e6fd147935532bed607ef8
David <deh0_@hotmail.com>
2020-06-08 22:44:17 -0300

    Implemetação dos métodos do managedBeanViewAbstract

--- src/br/com/project/bean/view/FuncionarioBeanView.java
+++ src/br/com/project/bean/view/FuncionarioBeanView.java
@@ -2,6 +2,7 @@ package br.com.project.bean.view;
 
 import javax.faces.bean.ManagedBean;
 
+import org.primefaces.model.StreamedContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -11,61 +12,134 @@ import br.com.project.bean.geral.BeanManagedViewAbstract;
 import br.com.project.carregamento.lazy.CarregamentoLazyListForObject;
 import br.com.project.geral.controller.EntidadeController;
 import br.com.project.model.classes.Entidade;
+import br.com.project.util.all.Messages;
 
 @Controller
 @Scope("session")
-@ManagedBean(name = "funcionarioBeanView")
-public class FuncionarioBeanView extends BeanManagedViewAbstract {
-
+@ManagedBean(name="funcionarioBeanView")
+public class FuncionarioBeanView extends BeanManagedViewAbstract{
+	
 	private static final long serialVersionUID = 1L;
+	
+	private String urlFind = "/cadastro/find_funcionario.jsf?faces-redirect=true";
+	
+	private String url = "/cadastro/cad_funcionario.jsf?faces-redirect=true"; 
 
-	private Entidade objetoSelecionado;
-
+	private Entidade objetoSelecionado = new Entidade();
+	
 	@Autowired
-	private ContextoBean cotextoBean;
-
+	private ContextoBean contextoBean;
+	
 	@Autowired
 	private EntidadeController entidadeController;
+	
+	private CarregamentoLazyListForObject<Entidade> list = new CarregamentoLazyListForObject<Entidade>();
 
-	private CarregamentoLazyListForObject<Entidade> list = new CarregamentoLazyListForObject<>();
-
+	
 	@Override
-	public void consultarEntidade() throws Exception {
-		objetoSelecionado = new Entidade();
-		list.clean();
-		list.setTotalRegistroConsulta(super.totalRegistroConsulta(), super.getSqlLazyQuery());
+	public StreamedContent getArquivoReport() throws Exception {
+		super.setNomeRelatorioJasper("report_funcionario");
+		super.setNomeRelatorioSaida("report_funcionario");
+		super.setListDataBeanCollectionReport(entidadeController.findList( getClassImplemt()));
+		return super.getArquivoReport();
 	}
-
+	
 	@Override
-	protected Class<?> getClassImplemt() {
+	protected Class<Entidade> getClassImplemt() {
 		return Entidade.class;
 	}
-
+	
+	
+	@Override
+	public String novo() throws Exception {
+		objetoSelecionado = new Entidade();
+		list.clean();
+		return url;
+	}
+	
+	
+	@Override
+	public void excluir() throws Exception {
+		if (objetoSelecionado.getEnt_codigo() != null
+				&& objetoSelecionado.getEnt_codigo() > 0 ){
+			entidadeController.delete(objetoSelecionado);
+			list.remove(objetoSelecionado);
+			objetoSelecionado = new Entidade();
+			sucesso();
+		}
+	}
+	
 	@Override
 	protected InterfaceCrud<?> getControler() {
-		// TODO Auto-generated method stub
 		return entidadeController;
 	}
 
 	@Override
 	public String condicaoAndParaPesquisa() throws Exception {
-		return " AND entity.tipoEntidade = 'FUNCIONARIO' ";
+		return " and entity.tipoEntidade = 'FUNCIONARIO' ";
 	}
-
+	
 	public void setObjetoSelecionado(Entidade objetoSelecionado) {
 		this.objetoSelecionado = objetoSelecionado;
 	}
-
+	
 	public Entidade getObjetoSelecionado() {
 		return objetoSelecionado;
 	}
-
+	
 	public void setList(CarregamentoLazyListForObject<Entidade> list) {
 		this.list = list;
 	}
-
 	public CarregamentoLazyListForObject<Entidade> getList() {
 		return list;
+	}
+	
+	
+	@Override
+	public String redirecionarFindEntidade() throws Exception {
+		list.clean();
+		return urlFind;
+	}
+	
+	@Override
+	public void consultarEntidade() throws Exception {
+		objetoSelecionado = new Entidade();
+		list.clean();
+		list.setTotalRegistroConsulta(super.totalRegistroConsulta(),
+				super.getSqlLazyQuery());
+		
+	}
+	
+	@Override
+	public void saveNotReturn() throws Exception {
+		if (!objetoSelecionado.getAcessos().contains("USER")) {
+			objetoSelecionado.getAcessos().add("USER");
+		}
+
+		if (entidadeController.existeCpf(objetoSelecionado.getCpf())) {
+			Messages.msgSeverityInfo("CPF já cadastrado");
+		} else {
+
+			objetoSelecionado = entidadeController.merge(objetoSelecionado);
+			list.add(objetoSelecionado);
+			objetoSelecionado = new Entidade();
+			sucesso();
+		}
+	}
+
+	@Override
+	public void saveEdit() throws Exception {
+		objetoSelecionado = entidadeController.merge(objetoSelecionado);
+		list.add(objetoSelecionado);
+		
+		objetoSelecionado = new Entidade();
+		Messages.msgSeverityInfo("Atualizado com sucesso!");
+	}
+	
+	@Override
+	public String editar() throws Exception {
+		list.clean();
+		return url;
 	}
 
 }
