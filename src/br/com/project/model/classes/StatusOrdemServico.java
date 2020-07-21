0f53082cd98ae271ce5bbdfc3bd1cf5efe565eb1
David <deh0_@hotmail.com>
2020-07-20 22:09:54 -0300

    Classe modelo de Status OrdemServico

---
+++ src/br/com/project/model/classes/StatusOrdemServico.java
@@ -0,0 +1,17 @@
+package br.com.project.model.classes;
+
+import java.io.Serializable;
+
+/*
+@Audited
+@Entity
+@Table(name = "status_os")
+@SequenceGenerator(name = "status_os_seq", sequenceName = "status_os_seq", initialValue = 1, allocationSize = 1)
+*/
+public class StatusOrdemServico implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+
+}
