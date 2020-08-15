3fc8066c07fd104926d833798b05116a3c1168f4
David <deh0_@hotmail.com>
2020-08-15 11:55:41 -0300

    Adicionado métodos para apontar e atualizar status da os

--- src/br/com/srv/implementacao/SrvApontamentoImpl.java
+++ src/br/com/srv/implementacao/SrvApontamentoImpl.java
@@ -3,7 +3,9 @@ package br.com.srv.implementacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.project.model.classes.Apontamento;
 import br.com.repository.interfaces.RepositoryApontamento;
+import br.com.repository.interfaces.RepositoryOperacaoProduto;
 import br.com.srv.interfaces.SrvApontamento;
 
 @Service
@@ -13,5 +15,20 @@ public class SrvApontamentoImpl implements SrvApontamento {
 	
 	@Autowired
 	RepositoryApontamento repositoryApontamento;
+	
+	//@Autowired
+	//RepositoryOperacaoProduto repositoryOperacaoProduto;
 
+	/*
+	@Override
+	public Apontamento apontar(Apontamento apontamento) throws Exception {
+		return repositoryApontamento.apontar(apontamento);
+	}
+
+	@Override
+	public Long atualizaStatusOs(Long idProduto, Long idOperacaoAtual) throws Exception {
+		return repositoryOperacaoProduto.proximaOperacao(idProduto, idOperacaoAtual);
+	}
+	*/
+	
 }
--- src/br/com/srv/interfaces/SrvApontamento.java
+++ src/br/com/srv/interfaces/SrvApontamento.java
@@ -4,7 +4,28 @@ import java.io.Serializable;
 
 import org.springframework.stereotype.Service;
 
+import br.com.project.model.classes.Apontamento;
+
 @Service
 public interface SrvApontamento extends Serializable{
+	//
+	/**
+	 * Realiza o apontamento de uma determinada ordem de serviço
+	 * @param apontamento
+	 * @return Apontamento
+	 * @throws Exception
+	 */
+	//Apontamento apontar(Apontamento apontamento) throws Exception;
+	
+	/**
+	 * Método para atualizar status da os.
+	 * @param idProduto
+	 * @param idOperacaoAtual
+	 * @return
+	 * @throws Exception
+	 */
+	//Long atualizaStatusOs(Long idProduto, Long idOperacaoAtual) throws Exception;
+	
+	
 
 }
