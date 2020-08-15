e54c1eefe8910d938eac10e9b647259f121cb6b7
David <deh0_@hotmail.com>
2020-08-15 11:54:38 -0300

    Método para consultar próxima op (Conecão com o banco não funciona no junit)

--- src/teste/junit/TesFuncionario.java
+++ src/teste/junit/TesFuncionario.java
@@ -1,48 +1,22 @@
 package teste.junit;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Calendar;
-
-import org.junit.Ignore;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
+import org.springframework.stereotype.Component;
 
-import br.com.project.geral.controller.EntidadeController;
-import br.com.project.model.classes.Entidade;
-import br.com.project.report.util.DataUtil;
+import br.com.framework.hibernate.session.HibernateUtil;
+import br.com.project.model.classes.OperacaoProduto;
 
+@Component
 public class TesFuncionario {
 	
-	EntidadeController entidadeController = new EntidadeController();
-	
-	
 	@Test
-	@Ignore
-	public void testDate() {
-		try {
-			assertEquals("03042020", DataUtil.getDateAtualReportName());
-			assertEquals("'2020-04-03'", DataUtil.formatDateSql(Calendar.getInstance().getTime()));
-			assertEquals("2020-04-03", DataUtil.formatDateSqlSimple(Calendar.getInstance().getTime()));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void testeOperacao(){
+		HibernateUtil.getSessionFactory().getCurrentSession().createCriteria(OperacaoProduto.class)
+		.add(Restrictions.eq("1".toString(), "1".toString()))
+		.setMaxResults(1).uniqueResult();
+		
 	}
 	
-	@Test
-	public void insertFuncionario() {
-		try {
-			Entidade entidade = new Entidade();
-			
-			entidade.setEnt_nomefantasia("David");
-			entidade.setEnt_login("admin");
-			entidade.setEnt_senha("admin");
-			
-			entidadeController.merge(entidade);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 }
