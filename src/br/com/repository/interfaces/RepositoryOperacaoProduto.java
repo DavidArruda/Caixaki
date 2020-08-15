db5ad75a57e3c8277da14da788ce1bc2c8724c91
David <deh0_@hotmail.com>
2020-08-15 11:56:32 -0300

    Adicionado método para  para consultar próxima op

--- src/br/com/repository/interfaces/RepositoryOperacaoProduto.java
+++ src/br/com/repository/interfaces/RepositoryOperacaoProduto.java
@@ -8,5 +8,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Repository
 public interface RepositoryOperacaoProduto extends Serializable {
+	
+	/**
+	 * Método para consultar a próxima operacao a ser apontanda. Retorna o id da próxima operação
+	 * @param produtoId
+	 * @param idOperacaoAtual
+	 * @return Long
+	 * @throws Exception
+	 */
+	Long proximaOperacao(Long produtoId, Long idOperacaoAtual) throws Exception;
 
 }
