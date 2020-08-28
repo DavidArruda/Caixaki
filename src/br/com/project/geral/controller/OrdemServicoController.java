5e7107eae1207c29371fd84f67c7b11bee80be58
David <deh0_@hotmail.com>
2020-08-28 14:40:50 -0300

    Adicionado métodas para consultar e setar status inicial de uma os

--- src/br/com/project/geral/controller/OrdemServicoController.java
+++ src/br/com/project/geral/controller/OrdemServicoController.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Controller;
 
 import br.com.framework.implementacao.crud.InplementacaoCrud;
 import br.com.framework.interfac.crud.InterfaceCrud;
+import br.com.project.model.classes.OperacaoProduto;
 import br.com.project.model.classes.OrdemServico;
 import br.com.repository.interfaces.RepositoryOrdemServico;
+import br.com.repository.interfaces.RepositoryStatusOs;
+import br.com.srv.interfaces.SrvOrdemServico;
 
 @Controller
 public class OrdemServicoController extends InplementacaoCrud<OrdemServico> implements InterfaceCrud<OrdemServico> {
@@ -16,9 +19,28 @@ public class OrdemServicoController extends InplementacaoCrud<OrdemServico> impl
 
 	@Resource
 	private RepositoryOrdemServico repositoryOrdemServico;
+	
+	@Resource
+	RepositoryStatusOs repositoryStatusOs;
+	
+	@Resource
+	OperacaoProdutoController operacaoProdutoController; 
+	
+	@Resource
+	private SrvOrdemServico srvOrdemServico;
 
-	public void setRepositoryOrdemServico(RepositoryOrdemServico repositoryOrdemServico) {
-		this.repositoryOrdemServico = repositoryOrdemServico;
+	public Long consultaIdOpInicial(String idProduto) throws Exception {
+		return srvOrdemServico.statusInicial(idProduto);
+	}
+	
+	/**
+	 * Consulta a operação inicial
+	 * @param idProduto
+	 * @return OperacaoProduto
+	 * @throws Exception
+	 */
+	public OperacaoProduto consultaOpInicial(String idProduto) throws Exception {
+		return  operacaoProdutoController.findObjById(OperacaoProduto.class, Long.parseLong(idProduto));
 	}
 
 }
