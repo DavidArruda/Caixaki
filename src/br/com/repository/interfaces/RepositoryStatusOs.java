8476214d60653f25e01f1f9a17eb43b66296c255
David <deh0_@hotmail.com>
2020-09-11 20:31:21 -0300

    Criado método save

--- src/br/com/repository/interfaces/RepositoryStatusOs.java
+++ src/br/com/repository/interfaces/RepositoryStatusOs.java
@@ -5,8 +5,12 @@ import java.io.Serializable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.project.model.classes.StatusO;
+
 @Transactional
 @Repository
 public interface RepositoryStatusOs extends Serializable {
+
+	void save(StatusO statusOs) throws Exception;
 	
 }
